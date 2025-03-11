b16677a0fed6e8df563787f51e4b8c06631ec5a4
unknown <danykeidel03@gmail.com>
2025-03-11 18:35:15 +0100

    node comandos

---
+++ NODE/MODULES/index.js
@@ -0,0 +1,10 @@
+const { suma, resta, division, multiplicacion } = require('./calc');
+
+console.log(suma(2, 3))
+console.log(resta(2, 3))
+console.log(multiplicacion(2, 3))
+console.log(division(2, 3))
+
+// npm uninstall jest
+// npm update
+//npm install express
