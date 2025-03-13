933a2d21cffc2527aba982a1888cceba22a30811
danielfernandez <danielfernandez@craftelier.com>
2025-03-13 13:37:58 +0100

    modulo archivos node

--- NODE/MODULES/index.js
+++ NODE/MODULES/index.js
@@ -10,8 +10,6 @@ console.log(division(2, 3))
 // npm install express
 // npm i
 
-
-
 const http = require('http')
 const server = http.createServer((req, res) => {
     res.writable(200, { 'Content-Type': 'text/plain' })
@@ -30,8 +28,87 @@ console.log(`Argumentos: ${process.argv}`);
 //MODULO REDLINE (pregunta por consola)
 
 const redline = require('readline')
-const rl = redline.createInterface({input: process.stdin, output: process.stdout})
+const rl = redline.createInterface({ input: process.stdin, output: process.stdout })
 rl.question('Como te llamas?', (nombre) => {
     console.log(`Hola, ${nombre}`);
-    rl.close()
+    rl.question('Edad?', (edad) => {
+        console.log(`Tu edad es: ${edad}`);
+        rl.close()
+    })
+})
+
+//MODULO PARA LEER Y ESCIRBIR DICHEROS Y CARPETAS
+
+const fs = require('fs');
+
+//Asincrono
+//fs.readFile(/** ruta del archivo, codificacion de lectura, callback */)
+
+//Escribir archivo (SI NO EXISTE LO CREA)
+
+fs.readFile('./prueba/escribir.txt', 'utf-8', (error, datos) => {
+    if (error) {
+        console.log(`Error: ${error}`);
+        return;
+    }
+    console.log(datos);
+})
+
+fs.writeFile('./prueba/escribir.txt', 'Esto es una prueba vale ?', 'utf-8', (error, datos) => {
+    if (error) {
+        console.log(`Error: ${error}`);
+        return;
+    }
+})
+
+fs.readFile('./prueba/escribir.txt', 'utf-8', (error, datos) => {
+    if (error) {
+        console.log(`Error: ${error}`);
+        return;
+    }
+    console.log(datos);
+})
+
+//ELIMINAR UN FICHERO
+
+fs.unlink('./prueba/prueba.txt', error => {
+    if(error) console.log(error);
+    console.log('ELiminado');
+})
+
+//CREAR Y ELIMINAR DIRECTORIO Y LEER
+
+fs.mkdir('./directorioPrueba', {recursive: true} , error => {
+    if(error) console.log(error);
+    console.log('Creado');
+})
+
+fs.rmdir('./directorioPrueba', error => {
+    if(error) console.log(error);
+    console.log('ELiminado');
+})
+
+fs.readdir('./prueba', (error, files) => {
+    if(error) console.log(error);
+    console.log(files);
+})
+
+//sacar info de archivos
+
+fs.stat('./prueba/escribir.txt', (error, datos) => {
+    if (error) {
+        console.log(`Error: ${error}`);
+        return;
+    }
+    console.log(datos);
+})
+
+//Copiar Ficheros
+
+fs.copyFile('./prueba/escribir.txt', './prueba/copiado/escribir.txt', (error, datos) => {
+    if (error) {
+        console.log(`Error: ${error}`);
+        return;
+    }
+    console.log(datos);
 })
