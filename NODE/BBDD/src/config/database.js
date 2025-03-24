acf58bdcfc2d77629ef97cf4a919044f92085199
unknown <danykeidel03@gmail.com>
2025-03-24 23:01:14 +0100

    laravel con node

--- NODE/BBDD/src/config/database.js
+++ NODE/BBDD/src/config/database.js
@@ -3,7 +3,7 @@ const mongose = require('mongoose')
 
 const connectBD = async () => {
     try{
-        await mongoose.connect('')
+        await mongoose.connect('mongodb+srv://danykeidel03:<db_password>@cluster0.yc5wo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         console.log('Conectado correctamente')
     }catch (e) {
         console.log('Database connection error: ', e)
